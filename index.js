f34c29b1b47b832b1fadb4295db9411b955524dd
Sameer Jawed <sameerzawed@gmail.com>
2023-11-12 12:58:07 +0530

    Update index.js
    update some js

--- index.js
+++ index.js
@@ -1,19 +1,33 @@
-const mobile_nav = document.querySelector(".mobile-navbar-btn");
-const nav_header = document.querySelector("header");
+// let typed = new Typed('.typedText', {
+//     Strings : ['Front-End Developer... ', 'Web-Developer...', 'Search Engine Optimization', 'UI & UX...'],
+// typedSpeed: 100,
+// backSpeed: 50,
+// backDelay:1000,
+// loop: true,
 
-const toggleNavbar = () => {
-//   alert("Plz Subscribe ");
-  nav_header.classList.toggle("active");
-};
+// });
 
-mobile_nav.addEventListener("click", () => toggleNavbar());
-
-var options = {
-    strings: ['It\'s Me your Sameer Jawed', '', ''],
-    typeSpeed: 50,
-    backSpeed: 50,
+var typed = new Typed(".auto",{
+    strings: ["Front-End Developer...", "Web-Developer...", "Search Engine Optimization", "UI & UX..."],
+    typedSpeed: 100,
+    backSpeed: 100,
+    backDelay:1000,
     loop: true
-    
+})
 
-};
-var Typed = new Typed('.typing', options); 
+
+// here we write code of light theme mood.
+let moon = document.querySelector('.moon');
+let sun = document.querySelector('.sun');
+
+moon.addEventListener('click',()=>{
+     sun.style.display= 'inline-block'
+     moon.style.display='none'
+     document.querySelector('body').classList.add('nightMood')
+});
+
+sun.addEventListener('click',()=>{
+     sun.style.display= 'none'
+     moon.style.display='inline-block'
+     document.querySelector('body').classList.remove('nightMood')
+});
